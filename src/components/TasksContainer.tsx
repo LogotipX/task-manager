b5b34c2d42456f241ff9e7a7546527a5fd9ad105
LogotipX <Starikov1000@gmail.com>
2022-04-15 10:06:28 +0500

    feat: add DragAndDrop containers

--- src/components/TasksContainer.tsx
+++ src/components/TasksContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import IssueBox from "./IssueBox";
 
 type TProps = {
@@ -36,29 +36,51 @@ export default function TasksContainer(props: TProps) {
 
   return (
     <>
-      <div
-        onMouseEnter={() => setVisibility("visible")}
-        onMouseLeave={() => setVisibility("invisible")}
-        className={`${props.className} task-container p-1 text-sm xs:w-screen w-72 text-slate-100 bg-slate-800 border-2 border-dashed border-slate-400 rounded-sm`}
-      >
-        <div className="container__name uppercase pl-2">
-          {props.containerName}
-        </div>
-        {IssueArr.map((i, idx) => (
-          <IssueBox
-            type={i.type}
-            title={i.title}
-            text={i.text}
-            className={i.className}
-            key={`${idx}-${i.type}-${i.title}`}
-          />
-        ))}
+      <DragDropContext onDragEnd={() => console.log("onDragEnd")}>
         <div
-          className={`button__create-issue ${visibility} px-1 py-2 mb-1 font-bold hover:bg-slate-600 active:bg-slate-700 cursor-pointer rounded-sm`}
+          onMouseEnter={() => setVisibility("visible")}
+          onMouseLeave={() => setVisibility("invisible")}
+          className={`${props.className} task-container p-1 text-sm xs:w-screen w-72 text-slate-100 bg-slate-800 border-2 border-dashed border-slate-400 rounded-sm`}
         >
-          + Create issue
+          <div className="container__name uppercase pl-2">
+            {props.containerName}
+          </div>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {IssueArr.map((i, idx) => (
+                  <Draggable
+                    key={`${idx}-${i.type}-${i.title}`}
+                    draggableId={`${idx}-${i.type}-${i.title}`}
+                    index={idx}
+                  >
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <IssueBox
+                          type={i.type}
+                          title={i.title}
+                          text={i.text}
+                          className={i.className}
+                          // key={`${idx}-${i.type}-${i.title}`}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+            )}
+          </Droppable>
+          <div
+            className={`button__create-issue ${visibility} px-1 py-2 mb-1 font-bold hover:bg-slate-600 active:bg-slate-700 cursor-pointer rounded-sm`}
+          >
+            + Create issue
+          </div>
         </div>
-      </div>
+      </DragDropContext>
     </>
   );
 }
